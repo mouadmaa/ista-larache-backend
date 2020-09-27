9c5dc7dd165562e5e5b76beee1b48f23be071a2c
mouadmaa <maaroufimouad00@gmail.com>
2020-09-27 15:19:50 +0100

    remove modules and classes when delete formation

--- src/resolvers/formationResolvers.ts
+++ src/resolvers/formationResolvers.ts
@@ -7,7 +7,13 @@ export const formations = (_parent: any, _args: any, { db }: MyContext): Promise
 
 export const createFormation = (_parent: any, args: FormationCreateArgs, { db }: MyContext): Promise<Formation> => db.formation.create(args)
 export const updateFormation = (_parent: any, args: FormationUpdateArgs, { db }: MyContext): Promise<Formation> => db.formation.update(args)
-export const deleteFormation = (_parent: any, args: FormationDeleteArgs, { db }: MyContext): Promise<Formation> => db.formation.delete(args)
+export const deleteFormation = async (_parent: any, args: FormationDeleteArgs, { db }: MyContext): Promise<Formation> => {
+  const deleteModules = db.module.deleteMany({ where: { formationId: args.where.id } })
+  const deleteClasses = db.class.deleteMany({ where: { formationId: args.where.id } })
+  const deleteFormation = db.formation.delete(args)
+  db.$transaction([deleteModules, deleteClasses, deleteFormation])
+  return deleteFormation
+}
 
 export const formationModules = (parent: FormationWhereUniqueInput, _args: any, { db }: MyContext): Promise<Module[]> => {
   return db.formation.findOne({ where: { id: parent.id } }).modules()
